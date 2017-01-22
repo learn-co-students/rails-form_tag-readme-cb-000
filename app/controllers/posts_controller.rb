1bbc37e08e3c7e39a8f9c2d47300be05989cb3f0
Derek Kaknes <derek.kaknes@gmail.com>
2017-01-22 17:40:30 -0500

    Submission

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,21 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def new
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      redirect_to posts_path
+    else
+      redirect_to new_posts_path
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
 end
