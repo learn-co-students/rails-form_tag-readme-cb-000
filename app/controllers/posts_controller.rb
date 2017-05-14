b9fb9780632b48ea9f47fd7f205f5fff90133431
jwald1 <email@example.com>
2017-05-14 16:44:44 -0400

    test passes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def new
+  end
+
+  def create
+    post = Post.new(title: params[:post][:title], description: params[:post][:description])
+    post.save
+
+    redirect_to posts_path
+  end
+
 end
