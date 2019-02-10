1b7733f3694dae7db1809813c34f841b82c94bf0
kenkuts <kennethchoi19@gmail.com>
2019-02-10 18:16:02 -0500

    commited

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,13 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
   def new
 
   end
-  
+
   def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
-    redirect_to post_path 
-  end 
+    redirect_to posts_path
+  end
 end
